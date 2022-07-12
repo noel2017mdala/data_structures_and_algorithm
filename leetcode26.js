6026a682194369f507dd9b91ef23c05e86b0a406
noel2017mdala <noelmdala2017@gmail.com>
2022-07-12 19:42:18 +0200

    leetcode number 26 solution

---
+++ leetcode26.js
@@ -0,0 +1,18 @@
+const removeDuplicates = (nums) =>{
+if(nums.length === 0){
+    return 0
+}else{
+    let pointer1 = 0;
+
+    for(let pointer2 = 1; pointer2 < nums.length; pointer2++){
+        if(nums[pointer1] !== nums[pointer2]){
+            pointer1++;
+            nums[pointer1] = nums[pointer2];
+        }
+    }
+
+    return pointer1 + 1
+}
+}
+
+console.log(removeDuplicates([1,1, 3, 4]));
